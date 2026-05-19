2a9662a4b9fbb49a8800b15caabc1704c9c12ee2
Ori Israel <oriisrael@Oris-MacBook-Air.local>
2026-05-19 17:37:12 +0300

    wrap sw cache install in try/catch

--- sw.js
+++ sw.js
@@ -14,7 +14,9 @@ const ASSETS = [
 
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+        caches.open(CACHE_NAME).then(async cache => {
+            try { await cache.addAll(ASSETS); } catch (e) { console.warn('[SW] cache install error:', e); }
+        })
     );
     self.skipWaiting();
 });
